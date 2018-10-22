46f7861baac1db33f635de2a0fb0c815470ecabc
forte <forte-bin@github.com>
2018-10-22 12:31:23 -0700

    added servers to the rack

--- controller.py
+++ controller.py
@@ -1,29 +1,34 @@
 import pickle
 from werkzeug.wrappers import Request, Response
 from werkzeug.utils import *
+import threading
 from classes import *
 # import traceback
 
 class Controller:
 	def __init__(self):
 		self.racks = []
+		self.rack_lock = threading.Lock()
+		self.io_lock = threading.Lock()
 		try: 
 			f = open('./racks.state','rb')
-			q=f.read()
-			print(q)
-			self.racks = pickle.loads(q)
+			self.racks = pickle.loads(f.read())
 		except Exception:
 			# traceback.print_stack()
 			print('failed to read file')
 		#TODO read in config/state file
 	
 	def save_state(self):
+		self.io_lock.acquire()
 		f = open('./racks.state','wb')
 		f.write(pickle.dumps(self.racks))
 		f.close()
+		self.io_lock.release()
 		
 	def add_rack(self, **kwargs):
+		self.rack_lock.acquire()
 		self.racks.append(Rack(kwargs['size']))
+		self.rack_lock.release()
 		
 	#only supports removing the last rack
 	def del_rack(self):
